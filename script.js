870d18a9cebf80fae64b56932f40ad696d91da6c
baylax816-lab <baylax816@gmail.com>
2026-04-05 01:01:56 -0500

    added to all the files including the script.js file and style file to make it look more like my colleges websites. I also made sure that all of them were linked together.

---
+++ script.js
@@ -0,0 +1,4 @@
+const dateEl = document.getElementById('date');
+const today = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+dateEl.textContent = today.toLocaleDateString('en-US', options);
